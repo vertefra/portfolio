ce22f1117f46b2653bcf31ab22e9fed1326686b8
vertefra <verte.fra@gmail.com>
2020-10-17 19:13:13 -0400

    getPort()

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from controllers import controller
-from setup import views
+from setup import Config
 import uvicorn
+import os
+
+
+PORT = Config.getPort(5000)
+print(PORT)
 
 app = FastAPI()
 app.include_router(
@@ -16,4 +21,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 print(__name__)
 if __name__ == "__main__":
     print("server running")
-    uvicorn.run("server:app", port=3000, host="127.0.0.1", reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=int(PORT))
