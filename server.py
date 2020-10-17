9e0392b6e97bad7b01a3d086a30899675c45a47d
vertefra <verte.fra@gmail.com>
2020-10-17 09:02:19 -0400

    templating setup completed

--- server.py
+++ server.py
@@ -13,4 +13,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 print(__name__)
 if __name__ == "__main__":
     print("server running")
-    uvicorn.run("server:pp", port=3000, host="127.0.0.1", reload=True)
+    uvicorn.run("server:app", port=3000, host="127.0.0.1", reload=True)
