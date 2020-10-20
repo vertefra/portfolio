45e67523ed778102f1b53d19b164dbc8824f5d78
vertefra <verte.fra@gmail.com>
2020-10-20 19:37:15 -0400

    merge

--- database.py
+++ database.py
@@ -3,25 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 from models import modelProject
-<<<<<<< HEAD
-import logging
-import os
-
-
-DEV_URL = "postgresql:///projects"
-PROD_URL = os.environ["DATABASE_URI"]
-
-
-engine = create_engine(PROD_URL)
-
-# if not database_exists(engine.url):
-#     create_database(engine.url)
-# else:
-#     print("Engine connected")
-#     engine.connect()
-
-engine.connect()
-=======
 from setup import project_config
 
 URL = project_config.DB_URL
@@ -33,7 +14,6 @@ if project_config.env == 'dev':
     else:
         print("Engine connected")
         engine.connect()
->>>>>>> 20d9debbd951306b7fff561f05c73dbc8efd1526
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
