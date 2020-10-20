861cd5f12d4311caac26779857c036e39bb60904
vertefra <verte.fra@gmail.com>
2020-10-20 10:03:23 -0400

    fix

--- database.py
+++ database.py
@@ -6,11 +6,13 @@ from models import modelProject
 import logging
 import os
 
+
+PROV_UL = "postgres://cdyfwyefnnwhmf:f24dcbf133be0b94a1aab80870e055e5d38491b9ab8ddd986b72c9c8f4ceedfd@ec2-54-158-190-214.compute-1.amazonaws.com:5432/da3qpb4pbp2f88"
 DEV_URL = "postgresql:///projects"
 PROD_URL = os.environ["DATABASE_URL"]
 
 
-engine = create_engine(PROD_URL)
+engine = create_engine(PROV_URL)
 
 if not database_exists(engine.url):
     create_database(engine.url)
