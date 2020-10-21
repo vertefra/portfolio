4e82fbb9116ecea553e9f84a55a81258da444138
vertefra <verte.fra@gmail.com>
2020-10-21 10:55:16 -0400

    footer added

--- setup.py
+++ setup.py
@@ -37,12 +37,8 @@ class Config():
 
                 if cmd == 'dev' or cmd == 'prod' or cmd == 'test':
                     self.env = cmd
-<<<<<<< HEAD
-
-=======
                     print("setting envionment to => ", self.env)
                     return self.env
->>>>>>> 1103f7fdba8bf8dc310cb520f8c47450708a7021
                 else:
                     print(f'option {cmd} not foond setting env=dev')
 
@@ -52,21 +48,10 @@ class Config():
 
     def def_setup(self, port=3000):
 
-<<<<<<< HEAD
-        if (self.env):
-
-            if self.env == 'dev':
-
-                self.PORT = port
-                self.DB_URL = "postgresql:///projects"
-
-            elif self.env == 'prod':
-=======
         if self.env == 'dev':
 
             self.PORT = port
             self.DB_URL = "postgresql:///projects"
->>>>>>> 1103f7fdba8bf8dc310cb520f8c47450708a7021
 
         elif self.env == 'prod':
 
@@ -77,10 +62,7 @@ class Config():
 print("===== SETTING EVIRONTMENT VARIABLES =====")
 project_config = Config()
 project_config.get_env()
-<<<<<<< HEAD
-=======
 
 print("current config: ")
 print("env = ", project_config.env)
->>>>>>> 1103f7fdba8bf8dc310cb520f8c47450708a7021
 project_config.def_setup(port=5000)
